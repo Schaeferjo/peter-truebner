cbc5622dfaf4e9ac8d1d733de33a33fecea4f11c
Schaeferjo <mail@joschaefer.com>
2020-06-25 22:24:38 +0200

    Created Lebenslauf component

---
+++ components/lebenslauf-eintrag.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cv-item">
+    <div class="cv-item-date">
+      {{ date }}
+    </div>
+
+    <div class="cv-item-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.cv-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+
+.cv-item-date {
+  text-align: right;
+  font-weight: bold;
+  font-size: 1.2em;
+  border-right: 1px solid #ccc;
+  padding: 2em;
+}
+
+.cv-item-content {
+  text-align: left;
+  padding: 2em;
+}
+</style>
--- pages/lebenslauf.vue
+++ pages/lebenslauf.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>lebenslauf</h1>
+  <div id="lebenlauf">
+    <h1>lebenslauf</h1>
+
+    <LebenslaufEintrag date="2020">
+      Professor für Bildungssoziologie an der Pädagogischen Hochschule der
+      Fachhochschule Nordwestschweiz.
+    </LebenslaufEintrag>
+  </div>
 </template>
+
+<script>
+import LebenslaufEintrag from '../components/lebenslauf-eintrag.vue'
+
+export default {
+  components: {
+    LebenslaufEintrag,
+  },
+}
+</script>
