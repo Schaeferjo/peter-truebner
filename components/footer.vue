1a7266f1b68487eb46d0d98165e451710933084d
Schaeferjo <mail@joschaefer.com>
2020-06-29 22:03:56 +0200

    Some footer css

--- components/footer.vue
+++ components/footer.vue
@@ -1,22 +1,30 @@
 <template>
-  <v-footer app>
-    <ul>
-      <li>Copyright {{ new Date().getFullYear() }}</li>
-      <li>Prof. em. Dr. Peter Trübner</li>
-      <li>Alexander Moserstraße 32a</li>
-      <li>CH-2503 Biel/Bienne</li>
-      <li>Schweiz</li>
-      <li>mail@petertruebner.ch</li>
-      <li>01234. 56789</li>
-    </ul>
-  </v-footer>
+  <footer app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" xs="12" sm="6" lg="3"
+          >Prof. em. Dr. phil. Peter Trübner</v-col
+        >
+        <v-col cols="12" xs="12" sm="6" lg="3">
+          Alexander Moserstraße 32a
+          <br />CH-2503 Biel/ Bienne <br />Schweiz
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" lg="3">
+          mail@petertruebner.ch
+          <br />123456789
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" lg="3"
+          >Copyright {{ new Date().getFullYear() }}</v-col
+        >
+      </v-row>
+    </v-container>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
-ul {
-  list-style-type: none;
-  li {
-    display: inline-block;
-  }
+.row {
+  display: flex;
 }
 </style>
+
+>
